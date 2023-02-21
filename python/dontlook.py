6ffe91174b4dd00404199697bc6b289c987d2a96
vicky <rigaut.theotime@gmail.com>
2023-02-21 22:26:45 +0100

    i'm not very focused on atomic commits

--- python/dontlook.py
+++ python/dontlook.py
@@ -1,24 +1,12 @@
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
 
 dpg.create_context()
-dpg.configure_app(manual_callback_management=True)
-dpg.create_viewport()
+dpg.create_viewport(title='Custom Title', width=600, height=600)
+
+demo.show_demo()
+
 dpg.setup_dearpygui()
-
-
-def callback(sender, app_data, user_data):
-    print("Called on the main thread!")
-
-
-with dpg.window(label="Tutorial"):
-    dpg.add_button(label="Press me", tag="pog", callback=callback)
-
-
-# main loop
 dpg.show_viewport()
-while dpg.is_dearpygui_running():
-    jobs = dpg.get_callback_queue()  # retrieves and clears queue
-    dpg.run_callbacks(jobs)
-    dpg.render_dearpygui_frame()
-
+dpg.start_dearpygui()
 dpg.destroy_context()
