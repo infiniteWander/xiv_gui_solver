14a51f208d5b52c2913311277a70ea87408c4e3c
vicky <rigaut.theotime@gmail.com>
2023-02-22 01:11:21 +0100

    reworked the whole logic behind the gui, this is starting to look decent

--- python/main.py
+++ python/main.py
@@ -9,67 +9,99 @@ dpg.create_viewport(title='XIV Solver', width=500, height=700)
 
 
 class User:
-    name = ''
+    name = 'NEW'
+    food = ''
+    pot = ''
+    specialist = False
+    initial_craftsmanship = 0
+    initial_control = 0
+    initial_cp = 0
     craftsmanship = 0
     control = 0
     cp = 0
-    food = ''
-    pot = ''
 
-    def set_user_stats(self, _, app_data, __):
-        self.craftsmanship = app_data[0]
-        self.control = app_data[1]
-        self.cp = app_data[2]
-        dpg.set_value(user_stats, user.get_user_stats())
-        return self.craftsmanship, self.control, self.cp
+    def refresh_gui(self) -> True:
+        self.compute_stats()
+        dpg.set_value(user_combo, self.name)
+        dpg.set_value(food_combo, self.food)
+        dpg.set_value(pot_combo, self.pot)
+        dpg.set_value(user_effective_stats, [self.craftsmanship, self.control, self.cp])
+        return True
 
-    def set_user_name(self, _, app_data, __):
-        self.name = app_data
-        self.set_user_stats(0, full_config.get_users_dict()[self.name], None)
-        dpg.set_value(user_stats, self.get_user_stats())
-        return self.name
+    def compute_stats(self) -> list:
+        food = []
+        try:
+            full_food = full_config.get_foods_dict()[self.food]
+            # [[5, 120], [0, 0], [26, 82]]
+            if user.initial_craftsmanship * full_food[0][0] / 100 > full_food[0][1]:
+                food.append(full_food[0][1])
+            else:
+                food.append(round(user.initial_craftsmanship * full_food[0][0] / 100))
+            if user.initial_control * full_food[1][0] / 100 > full_food[1][1]:
+                food.append(full_food[1][1])
+            else:
+                food.append(round(user.initial_control * full_food[1][0] / 100))
+            if user.initial_cp * full_food[2][0] / 100 > full_food[2][1]:
+                food.append(full_food[2][1])
+            else:
+                food.append(round(user.initial_cp * full_food[2][0] / 100))
+        except KeyError as _:
+            food = [0, 0, 0]
+            pass
 
-    def get_user_stats(self):
-        return self.craftsmanship, self.control, self.cp
+        pot = []
+        try:
+            full_pot = full_config.get_pots_dict()[self.pot]
+            # [(0, 0), (0, 0), (6, 21)]
+            if user.initial_craftsmanship * full_pot[0][0] / 100 > full_pot[0][1]:
+                pot.append(full_pot[0][1])
+            else:
+                pot.append(round(user.initial_craftsmanship * full_pot[0][0] / 100))
+            if user.initial_control * full_pot[1][0] / 100 > full_pot[1][1]:
+                pot.append(full_pot[1][1])
+            else:
+                pot.append(round(user.initial_control * full_pot[1][0] / 100))
+            if user.initial_cp * full_pot[2][0] / 100 > full_pot[2][1]:
+                pot.append(full_pot[2][1])
+            else:
+                pot.append(round(user.initial_cp * full_pot[2][0] / 100))
+        except KeyError as _:
+            pot = [0, 0, 0]
+            pass
 
-    def set_specialist(self, _, app_data, __):
+        if user.specialist:
+            specialist = [20, 20, 15]
+        else:
+            specialist = [0, 0, 0]
+
+        self.craftsmanship = self.initial_craftsmanship + food[0] + pot[0] + specialist[0]
+        self.control = self.initial_control + food[1] + pot[1] + specialist[1]
+        self.cp = self.initial_cp + food[2] + pot[2] + specialist[2]
+
+    def set_initial_stats(self, _, app_data, __) -> list:
+        self.initial_craftsmanship = app_data[0]
+        self.initial_control = app_data[1]
+        self.initial_cp = app_data[2]
+        self.refresh_gui()
+        return [self.initial_craftsmanship, self.initial_control, self.initial_cp]
+
+    def set_specialist(self, _, app_data, __) -> bool:
         if app_data:
-            self.craftsmanship += 20
-            self.control += 20
-            self.cp += 15
-            dpg.set_value(user_stats, [self.craftsmanship, self.control, self.cp, 0])
+            self.specialist = True
         else:
-            self.craftsmanship -= 20
-            self.control -= 20
-            self.cp -= 15
-            dpg.set_value(user_stats, [self.craftsmanship, self.control, self.cp, 0])
-        return self.craftsmanship, self.control, self.cp
+            self.specialist = False
+        self.refresh_gui()
+        return self.specialist
 
-    def set_food(self, un, app_data, nu):
-        # TODO: create pre- and post-enhancements stats to be able to handle multiple stacking buffs
+    def set_food(self, _, app_data, __) -> str:
         self.food = app_data
-        temp_user_craftsmanship = user.craftsmanship
-        temp_user_control = user.control
-        temp_user_cp = user.cp
+        self.refresh_gui()
+        return self.food
 
-        craftsmanship, control, cp = full_config.get_foods_dict()[app_data]
-
-        if user.craftsmanship * craftsmanship[0] / 100 > craftsmanship[1]:
-            temp_user_craftsmanship += craftsmanship[1]
-        else:
-            temp_user_craftsmanship += round(user.craftsmanship * craftsmanship[0] / 100)
-
-        if user.control * control[0] / 100 > control[1]:
-            temp_user_control += control[1]
-        else:
-            temp_user_control += round(user.control * control[0] / 100)
-
-        if user.cp * cp[0] / 100 > cp[1]:
-            temp_user_cp += cp[1]
-        else:
-            temp_user_cp += round(user.cp * cp[0] / 100)
-
-        dpg.set_value(user_stats, [temp_user_craftsmanship, temp_user_control, temp_user_cp])
+    def set_pot(self, _, app_data, __) -> str:
+        self.pot = app_data
+        self.refresh_gui()
+        return self.food
 
 
 class Recipe:
@@ -105,21 +137,25 @@ with dpg.window(label="Settings", width=500, height=700, no_resize=True, no_titl
     recipe_names = full_config.get_recipes_names()
 
     dpg.add_text("Character")
-    user_combo = dpg.add_combo(items=characters_names, label="Your character", callback=user.set_user_name)
+    user_combo = dpg.add_combo(items=characters_names, label="Your character")
     dpg.set_value(user_combo, "NEW")
     # TODO: Load 1rst user in users.yaml by default
-    # Make NEW prompt for a name when saving
-    user_stats = dpg.add_input_intx(size=3, label="Stats", tag="stats_tooltip", callback=user.set_user_stats)
+    # TODO: Make NEW prompt for a name when saving
+    user_stats = dpg.add_input_intx(size=3, label="Stats", tag="stats_tooltip", callback=user.set_initial_stats)
     with dpg.tooltip("stats_tooltip"):
         dpg.add_text("Craftsmanship / Control / CP")
-    specialist_checkbox = dpg.add_checkbox(label="Specialist", tag="specialist", callback=user.set_specialist)
     dpg.add_button(label="Save!", indent=282)
+    specialist_checkbox = dpg.add_checkbox(label="Specialist", tag="specialist", callback=user.set_specialist)
+    dpg.add_text(" ")
+    user_effective_stats = dpg.add_input_intx(size=3, label="Effective stats", tag="effective_stats_tooltip", readonly=True)
+    with dpg.tooltip("effective_stats_tooltip"):
+        dpg.add_text("Craftsmanship / Control / CP")
 
     dpg.add_separator()
 
     dpg.add_text("Consumables")
     food_combo = dpg.add_combo(items=foods_names, label="Food", callback=user.set_food)
-    pot_combo = dpg.add_combo(items=pots_names, label="Pot")
+    pot_combo = dpg.add_combo(items=pots_names, label="Pot", callback=user.set_pot)
 
     dpg.add_separator()
 
@@ -128,7 +164,7 @@ with dpg.window(label="Settings", width=500, height=700, no_resize=True, no_titl
     dpg.set_value(recipe_combo, "NEW")
     recipe_stats = dpg.add_input_intx(size=3, label="Stats", tag="recipe_tooltip", callback=recipe.set_recipe_stats)
     with dpg.tooltip("recipe_tooltip"):
-        dpg.add_text("Quality / Progress / Durability")
+        dpg.add_text("Quality / Difficulty / Durability")
     # TODO: add a small grey text message if recipe matches a known recipe
     dpg.add_button(label="Save!", indent=282)
 
